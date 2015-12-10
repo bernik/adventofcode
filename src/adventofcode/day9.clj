58fdda78f0ae5c8092b775077ad25faf36526dcb
Bernik <i.am.bernik@gmail.com>
2015-12-10 21:34:26 +0200

    day 9 bleat'

--- src/adventofcode/day9.clj
+++ src/adventofcode/day9.clj
@@ -1,7 +1,6 @@
 (ns adventofcode.day9
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]
-            [clojure.set :as set]))
+            [clojure.string :as str]))
 
 (def distances (->> "day9.txt"
                     io/resource
@@ -9,50 +8,43 @@
                     slurp
                     str/split-lines
                     (reduce #(let [[_ from to dist] (re-find #"(\w+) to (\w+) = (\d+)" %2)]
-                               (assoc-in %1 [from to] (read-string dist))) 
+                               (-> %1 
+                                   (assoc-in [from to] (read-string dist))
+                                   (assoc-in [to from] (read-string dist)))) 
                             {})))
 
-#_(def distances {:london {:dublin 464
-                           :belfast 518}
-                  :dublin {:belfast 141}})
-
-(def all-cities 
-  (set/union
-   (->> distances keys (into #{}))
-   (->> distances 
-        (map #(-> % second keys))
-        (reduce #(into %1 %2) #{}))))
-
-(defn next-destination [curr unvisited]
+(defn destination-by [criteria curr unvisited]
   (when-not (dead-end? curr unvisited)
     (->> (select-keys (get distances curr) unvisited)
-         (apply min-key second))))
+         (apply criteria second))))
 
 (defn dead-end? [curr unvisited]
   (or (not (contains? distances curr))
       (empty? (select-keys (get distances curr) unvisited))))
 
-(defn shortest-distance [from]
+(defn distance-by [criteria from]
   (loop [distance 0
          curr from
-         unvisited (disj all-cities from)
-         path (conj [] from)]
+         unvisited (disj (into #{} (keys distances)) from)]
     (if (empty? unvisited) 
       distance
-      (if-let [[city dist] (next-destination curr unvisited)]
+      (if-let [[city dist] (destination-by criteria curr unvisited)]
         (recur (+ distance dist)
                city
-               (disj unvisited city)
-               (conj path city))
-        {:dead-end path}))))
+               (disj unvisited city))
+        :dead-end))))
+
+(def shortest (partial distance-by min-key))
+(def longest (partial distance-by max-key))
 
 (defn part-one []
   (->> (keys distances)
-       (map shortest-distance)
-       #_(filter #(not= :dead-end %))
-       #_(apply min)))
+       (map shortest)
+       (filter #(not= :dead-end %))
+       (apply min)))
 
-(comment 
-  (part-one)
-  (clojure.pprint/pprint all-cities)
-  (clojure.pprint/pprint distances))
+(defn part-two []
+  (->> (keys distances)
+       (map longest)
+       (filter #(not= :dead-end %))
+       (apply max)))
