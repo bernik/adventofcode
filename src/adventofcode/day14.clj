d7572de56a74335b0fb85a2fcf68131e9a70c35c
Bernik <i.am.bernik@gmail.com>
2015-12-15 00:56:58 +0200

    day 14 working

--- src/adventofcode/day14.clj
+++ src/adventofcode/day14.clj
@@ -2,18 +2,44 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn parse-line [line]
-    (let [[_ name speed fly rest] (re-find #"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds")]
-        (list name (parse-srting speed) (parse-string fly) (parse-string rest))))
+(defn line->list [line]
+  (let [[_ name speed fly rest] (re-find #"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds" line)]
+    (list name (read-string speed) (read-string fly) (read-string rest))))
 
-(def reindeers (->> "day14.txt"
-                io/resource
-                io/file
-                slurp
-                str/split-lines
-                (reduce
-                    (fn [res [name speed fly rest]
-                         (assoc-in res name {:speed speed
-                                             :fly fly
-                                             :rest rest})])
-                    {})))
+(def input-reindeers (->> "day14.txt"
+                          io/resource
+                          io/file
+                          slurp
+                          str/split-lines
+                          (map line->list)
+                          (reduce
+                           (fn [m [name speed fly rest]]
+                             (assoc m name {:speed speed
+                                            :fly fly
+                                            :rest rest}))
+                           {})))
+
+(defn distance [reindeers name time]
+  (let [{:keys [speed fly rest]} (get reindeers name)
+        intervals-count (quot time (+ fly rest))
+        time-remain (rem time (+ fly rest))]
+    (+
+     (* speed fly intervals-count)
+     (* speed (min time-remain fly)))))
+
+(defn winner [reindeers time]
+  (->> (keys reindeers)
+       (reduce #(assoc %1 %2 (distance reindeers %2 time))
+               {})
+       (sort-by second >)
+       first
+       first))
+
+(def part-one (->> (keys input-reindeers) 
+                   (map #(distance input-reindeers % 2503))
+                   (apply max)))
+
+(def part-two (->> (for [i (range 1 2504)] (winner input-reindeers i))
+                   frequencies
+                   (apply max-key val)
+                   second))
