7ae2bef39bc454c4b40091eda68ef0413771ba64
Bernik <i.am.bernik@gmail.com>
2015-12-15 23:46:44 +0200

    day 15

--- src/adventofcode/day15.clj
+++ src/adventofcode/day15.clj
@@ -18,15 +18,45 @@
                       str/split-lines
                       (map parse-ingredient)))
 
-(defn update-vals [m vals f]
-  (reduce #(update-in %1 [%2] f) m vals))
+(def proportions
+  (for [p1 (range 0 101)
+        p2 (range 0 101)
+        p3 (range 0 101)
+        p4 (range 0 101)
+        :when (= 100 (+ p1 p2 p3 p4))]
+    [p1 p2 p3 p4]))
 
-(comment (update-vals {:foo 1 :bar 2} [:foo :bar] (partial * 10)))
+(defn score [ingredient]
+  (->> (select-keys ingredient [:capacity :durability :flavor :texture])
+       vals
+       (map (fn [n] (if (> n 0) n 0)))
+       (apply *)))
 
-(defn get-proportions []
-  (for [sprinkles (range 0 101)
-        butterscotch (range 0 101)
-        chocolate (range 0 101)
-        candy (range 0 101)
-        :when (= 100 (+ sprinkles butterscotch chocolate candy))]
-    [sprinkles butterscotch chocolate candy]))
+(defn apply-proportions [ingredients proportions]
+  (->> ingredients 
+       (interleave proportions)
+       (partition 2)
+       (map (fn [[proportion ingredient]]
+              (reduce #(update %1 %2 (partial * proportion))
+                      ingredient
+                      [:capacity :durability :flavor :texture :calories])))
+       (map #(-> % (dissoc :name)))
+       (reduce #(-> %1
+                    (update :capacity (partial + (:capacity %2)))
+                    (update :durability (partial + (:durability %2)))
+                    (update :flavor (partial + (:flavor %2)))
+                    (update :texture (partial + (:texture %2)))
+                    (update :calories (partial + (:calories %2)))))))
+
+(def part-one 
+  (->> proportions
+       (map #(apply-proportions ingredients %))
+       (map score)
+       (apply max)))
+
+(def part-two
+  (->> proportions
+       (map #(apply-proportions ingredients %))
+       (filter #(= (:calories %) 500))
+       (map score)
+       (apply max)))
