65ac2d9888e8113fa87d0f3cd7ebeeafb9f7b2fa
Bernik <i.am.bernik@gmail.com>
2015-12-21 18:41:40 +0200

    day20 still not working

--- src/adventofcode/day20.clj
+++ src/adventofcode/day20.clj
@@ -1,9 +1,35 @@
-(ns adventofcode.day20)
+(ns adventofcode.day20
+  (:require [clojure.math.combinatorics :as combo]))
 
-(defn dividers [n]
+(defn get-dividers [n]
   (filter #(zero? (mod n %)) (range 1 (inc n))))
 
-(def part-one (->> (range)
-                   (map #(vector % (* 10 (apply + (dividers %)))))
-                   (filter #(= 29000000 (second %)))
-                   first))
+(comment (combo/subsets (reverse (dividers 10))))
+
+(def dividers (get-dividers 2900000))
+
+(def valid? (memoize (fn [subset] (= 2900000 (apply + subset)))))
+
+(defn home-number [n]
+  (let [part (take n dividers)]
+    (println n)
+    (some->> part
+             combo/subsets
+             (drop 1) ;; empty subset 
+             (filter #(.contains % (last part)))
+             (filter valid?)
+             first
+             (apply max))))
+
+(comment (def part-one (->> (dividers 2900000)
+                            reverse
+                            combo/subsets
+                            (drop 1)
+                            (map (juxt #(apply max %) #(apply + %)))
+                            (filter #(= 2900000 (second %)))
+                            first)))
+
+(comment (->> (range 1 (count dividers))
+              (map home-number)
+              (drop-while nil?)
+              first))
