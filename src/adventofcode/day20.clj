01b96bdb1de30b278ab54535150ce82e944385e5
Bernik <i.am.bernik@gmail.com>
2015-12-22 08:50:15 +0200

    day 20 part 1

--- src/adventofcode/day20.clj
+++ src/adventofcode/day20.clj
@@ -1,35 +1,16 @@
-(ns adventofcode.day20
-  (:require [clojure.math.combinatorics :as combo]))
+(ns adventofcode.day20)
 
-(defn get-dividers [n]
-  (filter #(zero? (mod n %)) (range 1 (inc n))))
+(defn divisors [number]
+  (mapcat (fn [divisor]
+            (when (zero? (mod number divisor))
+              (let [multiple (/ number divisor)]
+                (if (= multiple divisor)
+                  [divisor]
+                  [divisor multiple])))) 
+          (range 1 (inc (int (Math/sqrt (int number)))))))
 
-(comment (combo/subsets (reverse (dividers 10))))
-
-(def dividers (get-dividers 2900000))
-
-(def valid? (memoize (fn [subset] (= 2900000 (apply + subset)))))
-
-(defn home-number [n]
-  (let [part (take n dividers)]
-    (println n)
-    (some->> part
-             combo/subsets
-             (drop 1) ;; empty subset 
-             (filter #(.contains % (last part)))
-             (filter valid?)
-             first
-             (apply max))))
-
-(comment (def part-one (->> (dividers 2900000)
-                            reverse
-                            combo/subsets
-                            (drop 1)
-                            (map (juxt #(apply max %) #(apply + %)))
-                            (filter #(= 2900000 (second %)))
-                            first)))
-
-(comment (->> (range 1 (count dividers))
-              (map home-number)
-              (drop-while nil?)
-              first))
+(def part-one (->> (range)  
+                   (map #(vector % (apply + (divisors %))))
+                   (drop-while #(> 2900000 (second %)))
+                   first
+                   first))
