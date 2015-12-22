f8a97c97359befe04f04564c210340822fd84714
Bernik <i.am.bernik@gmail.com>
2015-12-22 09:13:22 +0200

    day 20

--- src/adventofcode/day20.clj
+++ src/adventofcode/day20.clj
@@ -9,8 +9,25 @@
                   [divisor multiple])))) 
           (range 1 (inc (int (Math/sqrt (int number)))))))
 
-(def part-one (->> (range)  
-                   (map #(vector % (apply + (divisors %))))
-                   (drop-while #(> 2900000 (second %)))
-                   first
-                   first))
+(defn presents-one [number]
+  (* 10 (apply + (divisors number))))
+
+(defn presents-two [number] 
+  (->> (divisors number)
+       (filter #(<= (/ number %) 50))
+       (map #(* 11 %))
+       (apply +)))
+
+(defn part-one [] 
+  (->> (range)  
+       (map #(vector % (presents-one %)))
+       (drop-while #(> 29000000 (second %)))
+       first
+       first))
+
+(defn part-two [] 
+  (->> (range)  
+       (map #(vector % (presents-two %)))
+       (drop-while #(> 29000000 (second %)))
+       first
+       first))
