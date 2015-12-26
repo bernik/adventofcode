ed94ccd00425b2ea39159e314ff1a3d6a7d9377a
Bernik <i.am.bernik@gmail.com>
2015-12-26 17:14:50 +0200

    day 24

--- src/adventofcode/day24.clj
+++ src/adventofcode/day24.clj
@@ -1,18 +1,23 @@
 (ns adventofcode.day24
   (:require [clojure.math.combinatorics :as combo]))
 
-(def presents #{1 2 3 5 7 13 17 19 23 29 31 37 41 43 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113})
+(def presents [1 2 3 5 7 13 17 19 23 29 31 37 41 43 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113])
+(def presents-count (count presents))
+(def total-weight (apply + presents))
 
-#_(def part-one (->> (combo/partitions presents :min 3 :max 3)
-                     count
-                     #_(filter #(and 
-                                 (every? (fn [coll] (< 1 (count coll))) %)
-                                 (apply = (map (partial apply +) %))))))
+(combo/combinations presents 4)
+(defn min-qe [parts n] 
+  (let [qes (->> (combo/combinations presents n)  
+                 (filter #(= (apply + %) (/ total-weight parts)))
+                 (pmap #(apply * %)))]  
+    (when-not (empty? qes) 
+      (apply min qes))))
 
-(comment (filter #(->> % 
-                       (map (partial apply +))
-                       (apply =)) 
-                 [[[1] [2] [3]]
-                  [[1 2] [0 3] [3]]])
+(defn answer [parts] 
+  (loop [n 0]
+    (if-let [qe (min-qe parts n)]
+      qe
+      (recur (inc n)))))
 
-         (count (take-while #(< (count %) 4) (combo/subsets (into [] presents)))))
+(def part-one (answer 3))
+(def part-two (answer 4))
