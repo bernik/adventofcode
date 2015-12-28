0b2e2a0b0678fbb21d304037e8155a38eaa37792
Bernik <i.am.bernik@gmail.com>
2015-12-28 09:07:59 +0200

    day22 still no idea how to solve

--- src/adventofcode/day22.clj
+++ src/adventofcode/day22.clj
@@ -1,5 +1,4 @@
-(ns adventofcode.day22
-  (:require [clojure.math.combinatorics :as combo]))
+(ns adventofcode.day22)
 
 (def boss {:hp 71
            :damage 10})
@@ -9,7 +8,7 @@
 
 (def spells [{:name :magic-missile
               :cost 53 
-              :damage 2}
+              :damage 4}
              {:name :drain
               :cost 73
               :damage 2 
@@ -26,44 +25,3 @@
               :cost 229
               :mana 101
               :turns 5}])
-
-(defn total-damage [spells]
-  (apply + (map #(* (get % :damage 0) (get % :turns 1)) spells)))
-
-(defn total-heal [spells]
-  (apply + (map #(* (get % :heal 0) (get % :turns 1)) spells))) 
-
-(defn total-cost [spells]
-  (- (apply + (map #(* (get % :mana 0) (get % :turns 1)) spells))
-     (apply + (map :cost spells))))  
-
-(defn total-spend-mana [spells]
-  (apply + (map #(get % :cost) spells)))
-
-(comment 
-  (wizard {:hp 10 :mana 250} [3 0])
-  (total-damage (map #(get spells %) [3 0])) 
-  (total-heal (map #(get spells %) [3 0])) 
-  (total-cost (map #(get spells %) [3 0]))
-  (total-spend-mana (map #(get spells %) [3 0])))
-
-(defn wizard [me spell-indexes]
-  (let [spells (map #(get spells %) spell-indexes)]
-    (-> me 
-        (update :hp + (total-heal spells))
-        (update :mana + (total-cost spells))
-        (assoc :damage (total-damage spells)))))
-
-(defn win? [me boss spell-indexes]  
-  (let [wizard (wizard me spell-indexes)
-        boss-damage (* (:damage boss) (dec (count spell-indexes)))] 
-    (and (>= (:mana wizard) 0)
-         (>= (- (:hp wizard) boss-damage)
-             (- (:hp boss) (:damage wizard))))))
-
-(defn min-win [spells-count]
-  (->> (combo/selections (range 0 5) spells-count)
-       (filter #(win? me boss %))
-       (map #(get spells %))
-       (map total-spend-mana)
-       (apply min)))
