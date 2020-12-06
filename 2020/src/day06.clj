ee0840627093518817ad5567f622649432fb3f09
Bernik <i.am.bernik@gmail.com>
2020-12-06 17:25:14 +0200

    aoc2020 day 06

---
+++ 2020/src/day06.clj
@@ -0,0 +1,27 @@
+(ns day06
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.set :as set]))
+
+(def input (-> "day06.txt"
+               io/resource
+               io/file
+               slurp
+               (str/split #"\n\n")))
+
+;; part 1 
+(->> input 
+    ;  (map parse-group-1)
+     (map #(count (disj (set %) \newline)))
+     (apply +)
+     println)
+
+;; part 2 
+(->> input
+     (map #(->> %
+                str/split-lines
+                (map set)
+                (apply set/intersection)
+                count))
+     (apply +)
+     println)
