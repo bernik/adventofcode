3cb732f47f385ecae6ae2810dd1ad6c0cddaa99d
bernik <i.am.bernik@gmail.com>
2020-12-18 11:44:50 +0200

    aoc2020 day17

--- 2020/src/day17.clj
+++ 2020/src/day17.clj
@@ -2,34 +2,39 @@
   (:refer-clojure :exclude [cycle])
   (:require [clojure.pprint :refer [pprint]]))
 
-(def test-input-raw [".#." 
-                     "..#" 
-                     "###"])
-(def test-input 
-  (->> test-input-raw 
+(def test-input [".#."
+                 "..#"
+                 "###"])
+
+(def input ["..#....#"
+            "##.#..##"
+            ".###...."
+            "#....#.#"
+            "#.######"
+            "##.#...."
+            "#......."
+            ".#......"])
+
+(defn to-db [input] 
+  (->> input 
        (map-indexed (fn [y line]
                       (->> line
-                           (map-indexed (fn [x active] [[x y 0] (= active \#)]))
-                           #_(mapcat (fn [[[x y] active]]
-                                     [[[x y -1] active]
-                                      [[x y  0] active]
-                                      [[x y  1] active]]))
+                           (map-indexed (fn [x active] [[x y 0 0] (= active \#)]))
                            (into []))))
        (mapcat identity)
        (into {})))
 
-; (pprint test-input)
+(defn neigbours [db [x y z w] part2?]
+  (for [x' [(dec x) x (inc x)]
+        y' [(dec y) y (inc y)]
+        z' [(dec z) z (inc z)]
+        w' (if part2? [(dec w) w (inc w)] [w])
+        :when (not= [x y z w] [x' y' z' w'])
+        :let [active' (get db [x' y' z' w'])]]
+    [[x' y' z' w'] active']))
 
-(defn neigbours [db [x y z]]
-  (for [x' (range (dec x) (+ 2 x))
-        y' (range (dec y) (+ 2 y))
-        z' (range (dec z) (+ 2 z))
-        :when (not= [x y z] [x' y' z'])
-        :let [active' (get db [x' y' z'])]]
-    [[x' y' z'] active']))
-
-(defn next-cube [db [point active]]
-  (let [c (->> (neigbours db point)
+(defn next-cube [db [point active] part2?]
+  (let [c (->> (neigbours db point part2?)
                (filter second)
                count)
         active' (if active
@@ -38,21 +43,31 @@
     [point active']))
 
 
-(defn cycle [db]
+(defn cycle [db part2?]
   (let [[x1 x2] (apply (juxt min max) (map #(nth % 0) (keys db)))
         [y1 y2] (apply (juxt min max) (map #(nth % 1) (keys db)))
-        [z1 z2] (apply (juxt min max) (map #(nth % 2) (keys db))) ]
+        [z1 z2] (apply (juxt min max) (map #(nth % 2) (keys db))) 
+        [w1 w2] (apply (juxt min max) (map #(nth % 3) (keys db))) ]
     (->> (for [x (range (dec x1) (+ 2 x2))
                y (range (dec y1) (+ 2 y2))
-               z (range (dec z1) (+ 2 z2))]
-           (next-cube db [[x y z] (get db [x y z])]))
+               z (range (dec z1) (+ 2 z2))
+               w (range (dec w1) (+ 2 w2)) ]
+           (next-cube db [[x y z w] (get db [x y z w])] part2?))
          (into {}))))
 
-(pprint (->> test-input
-            (iterate cycle)
-            (drop 6)
-            first
-            ;;  cycle
-             (filter second)
-             count
-             ))
+(defn boot-cycle [input part2?]
+  (->> input
+       to-db
+       (iterate #(cycle % part2?))
+       (drop 6)
+       first
+       (filter second)
+       count))
+
+;; part 1
+(pprint (boot-cycle test-input false))
+(pprint (boot-cycle input false))
+
+;; part 2
+(pprint (boot-cycle test-input true))
+(pprint (boot-cycle input true))
