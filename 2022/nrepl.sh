dcc00ad145217bbf84d92f09b23b20e476e67ccf
Bernik <i.am.bernik@gmail.com>
2022-11-30 23:06:40 +0200

    2022 deps

--- 2022/nrepl.sh
+++ 2022/nrepl.sh
@@ -1,4 +1,7 @@
-#!/bin/bash -euo pipefail
+#!/usr/bin/env bash 
+
+set -euo pipefail
+
 cd "`dirname $0`"
 
 clj -A:nrepl -M -m nrepl.cmdline --interactive
--- 2022/src/adventofcode2022/day01.clj
+++ 2022/src/adventofcode2022/day01.clj
@@ -1,1 +1,2 @@
 (ns adventofcode2022.day01)
+
