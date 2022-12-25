2542fb4232d0281c3b6faaa457aec495383f23cc
Bernik <i.am.bernik@gmail.com>
2022-12-26 01:02:07 +0200

    2022 day12

--- 2022/src/adventofcode2022/day12.clj
+++ 2022/src/adventofcode2022/day12.clj
@@ -2,96 +2,83 @@
   (:require [clojure.java.io :as io]))
 
 
-(def START -1)
-(def END (-> (int \z) (- (int \a)) inc))
-
-
 (defn parse-input [filename]
   (->> (io/resource filename)
        io/reader
        line-seq
-       (mapv (fn [line]
-               (mapv #(case %
-                       \S START
-                       \E END
-                       (-> (int %) (- (int \a))))
-                     line)))))
+       (mapv vec)))
 
 
 (def test-input (parse-input "day12.test.txt"))
 (def input (parse-input "day12.txt"))
 
 
+(defn ->int [c]
+  (case c
+    \S (int \a)
+    \E (int \z)
+    (int c)))
+
+
 (defn neighbours [m [row col]]
-  (let [v (get-in m [row col])]
+  (let [v (->int (get-in m [row col]))]
     (->> [[-1 0] [1 0] [0 -1] [0 1]]
-      (map #(vector (+ row (second %)) (+ col (first %))))
+      (map #(vector (+ row (first %)) (+ col (second %))))
       (filter #(and (get-in m %) 
-                    (#{0 1} (abs (- (get-in m %) v))))))))
+                    (<= v (inc (->int (get-in m %)))))))))
 
 
-(defn find-node [m v]
-  (->> (for [row (range (count m))
-             col (range (count (first m)))
-             :let [v' (get-in m [row col])]
-             :when (= v v')]
-         [row col])
-       first))
+(defn find-nodes [m v]
+  (for [row (range (count m))
+        col (range (count (first m)))
+        :let [v' (get-in m [row col])]
+        :when (= v v')]
+    [row col]))
 
 
-(let [m           test-input
-      start-node  (find-node m START)
-      end-node    (find-node m END)]
-  (loop [distances  {end-node 0}
-         queue      [end-node]
-         visited    #{}]
-    (cond 
-      (nil? queue)
-      distances
+(defn bfs [input]
+  (let [end-node (first (find-nodes input \E))]
+    (loop [distances  {end-node 0}
+           queue      [end-node]
+           visited    #{}]
+      (cond 
+        (empty? queue)
+        distances
+
+        (visited (first queue))
+        (recur distances (vec (rest queue)) visited)
+
+        :else 
+        (let [curr        (first queue)
+              distance'   (inc (distances curr))
+              neighbours' (neighbours input curr)
+              distances'  (->> neighbours'
+                               (reduce 
+                                 (fn [ds n]
+                                   (update 
+                                     ds 
+                                     n 
+                                     (fnil #(min % distance') Long/MAX_VALUE)))
+                                 distances))
+              queue'      (->> (concat (rest queue) neighbours')
+                               (into []))]
+          (recur distances' queue' (conj visited curr)))))))
 
 
-      ; (= node end-node)
-      ; (get distances end-node)
-
-      :else 
-      (let [distance'   (inc (distances node))
-            queue'      (->> queue
-                             (map (partial neighbours m node))
-                             (filter #(not (visited %))))
-            distances'  (->> neighbours'
-                             (reduce (fn [ds n]
-                                       (update ds n (fnil #(min % distance') Long/MAX_VALUE)))
-                                     distances))
-            nodes'      (->> (concat nodes neighbours')
-                             (sort-by #(get distances' % Long/MAX_VALUE)))]
-        (recur distances'
-               nodes'
-               (conj visited node)))
-      )))
+(defn part-1 [input]
+  (get (bfs input) (first (find-nodes input \S))))
 
 
-
-(let [m           test-input
-      start-node  (find-node m START)
-      end-node    (find-node m END)]
-  (loop [[path & queue] (list (list start-node))
-         visited        #{}]
-    (cond 
-      (nil? path)
-      :not-found
-
-      (= end-node (first path))
-      (reverse path)
-
-      :else 
-      (let [node (first path)
-            neighbours' (->> (neighbours m node)
-                             (filter #(not (visited %))))
-            visited' (into visited neighbours')
-            queue' (->> neighbours'
-                        (map #(conj path %))
-                        (into queue))]
-        (recur queue' visited')))))
+(defn part-2 [input]
+  (let [a-set (set (find-nodes input \a))]
+    (->> (bfs input)
+      (filter #(a-set (first %)))
+      vals
+      (apply min))))
 
 
-(into (list 1 2) (list 3 4))
+(comment 
+  (part-1 test-input)
+  (part-1 input)
+  (part-2 test-input)
+  (part-2 input))
