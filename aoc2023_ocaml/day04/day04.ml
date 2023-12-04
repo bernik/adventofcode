65e545e2e48a723119a0e6107a15fd1260bc2740
Bernik <i.am.bernik@gmail.com>
2023-12-04 22:31:08 +0200

    2023 day04 part2

--- aoc2023_ocaml/day04/day04.ml
+++ aoc2023_ocaml/day04/day04.ml
@@ -1,11 +1,24 @@
 open Core 
 
-let print_int_list l =
-    l
+(* utils *)
+
+let pf = Printf.printf;;
+
+let print_int_list lst =
+    lst
     |> List.map ~f:Int.to_string
     |> String.concat ~sep:", "
-    |> Printf.printf "(%s)\n"
+    |> pf "(%s)\n"
 ;;
+let print_int_arr arr =
+    arr
+    |> Array.to_list
+    |> List.mapi ~f:(fun idx n -> Printf.sprintf "%d => %d" idx n)
+    |> String.concat ~sep:", \n"
+    |> pf "(%s)\n"
+;;
+
+(* solution *)
 
 let parse file =     
     Aoc.read_lines file
@@ -20,29 +33,45 @@ let parse file =
     )
 ;;
 
+let matching_numbers (winning, my) = 
+    my    
+    |> List.filter ~f:(fun n -> List.mem winning n ~equal:Int.equal)
+    |> List.length 
+;;
+
 
 let part1 file = 
     parse file 
-    |> List.map ~f:(fun (winning, my) ->
-        let res = my
-            |> List.filter ~f:(fun n -> List.mem winning n ~equal:Int.equal)
-        in 
-        match List.length res with 
-        | 0 -> 0
-        | n -> Int.pow 2 (n - 1)
+    |> List.map ~f:matching_numbers
+    |> List.fold ~init:0 ~f:(fun acc n -> 
+        match n with
+            | 0 -> acc 
+            | x -> acc + Int.pow 2 (x - 1)
     )
-    |> List.fold ~init:0 ~f:( + )
     |> Int.to_string
 ;;
 
-let part2 file = "part2"
-
+let part2 file =
+    let cards = parse file |> List.map ~f:matching_numbers in
+    let init = Array.create ~len:(List.length cards) 1 in
+    cards 
+    |> List.foldi ~init ~f:(fun idx acc n -> 
+        match n with
+        | 0 -> acc
+        | _ -> 
+            List.range (idx + 1) (idx + n + 1)
+            |> List.iter ~f:(fun acc_idx -> acc.(acc_idx) <- (acc.(acc_idx) + acc.(idx)));
+            acc
+    )
+    |> Array.fold ~init:0 ~f:( + )
+    |> Int.to_string
+;;
 
 
 let () = 
-    Printf.printf "part1 example: %s\n" (part1 "day04/input.example.txt");
-    Printf.printf "part1: %s\n" (part1 "day04/input.txt");
-    (* Printf.printf "part2 example: %s\n" (part2 "day04/input.example.txt"); *)
-    (* Printf.printf "part2: %s\n" (part2 "day04/input.txt"); *)
+    pf "part1 example: %s\n" (part1 "day04/input.example.txt");
+    pf "part1: %s\n"         (part1 "day04/input.txt");
+    pf "part2 example: %s\n" (part2 "day04/input.example.txt");
+    pf "part2: %s\n"         (part2 "day04/input.txt");
     ();
 
