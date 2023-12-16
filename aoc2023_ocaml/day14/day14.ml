3ee5b14531d36b2a1143f205c7bc80980a0c6398
Bernik <i.am.bernik@gmail.com>
2023-12-17 01:48:58 +0200

    2023 day14 part2

--- aoc2023_ocaml/day14/day14.ml
+++ aoc2023_ocaml/day14/day14.ml
@@ -1,30 +1,99 @@
 open Base
 open Aoc
+open Stdio
 
-let parse file = 
-    Stdio.In_channel.read_lines file
-    |> List.map ~f:String.to_list
-;;
+module Tiles = struct 
+    module T = struct 
+        type t = char list list
+        [@@deriving compare, sexp_of, hash]
+
+
+        let of_file path =
+            In_channel.read_lines path
+            |> List.filter ~f:(fun x -> not @@ String.is_empty x)
+            |> List.map ~f:String.to_list
+        ;;
+
+
+        let total_load tiles = 
+            List.rev tiles 
+            |> List.foldi ~init:0 ~f:(fun idx acc xs -> 
+                let n = xs
+                    |> List.filter ~f:(Char.equal 'O') 
+                    |> List.length 
+                in
+                acc + (idx + 1) * n
+            )
+        ;;
+
+
+        let move tiles = 
+            List.map tiles ~f:(fun xs ->
+                let (res, acc) = List.fold xs ~init:([], []) ~f:(fun (res, acc) x -> 
+                    match x with 
+                    | 'O' -> res, 'O'::acc 
+                    | '.' -> res, (acc @ ['.']) 
+                    | '#' -> res @ acc @ ['#'], []
+                    | x -> failwith ("Invalid value" ^ (Char.to_string x))
+                ) in
+                res @ acc
+            )
+        ;;
+
+        let cycle tiles = 
+            let t = List.transpose_exn in
+            let r = List.rev in
+            tiles 
+            |> t |> move (* north *)
+            |> t |> move (* west *)
+            |> r |> t |> move (* south *)
+            |> r |> t |> move (* east *)
+            |> t |> r |> t |> r  (* init *)
+        ;;
+
+        let print tiles = 
+            List.iter tiles ~f:(fun row -> print_endline (String.of_char_list row))
+        ;;
+    end 
+    include T
+    include Comparator.Make(T)
+end
 
 
 let part1 file = 
-    parse file 
+    Tiles.of_file file 
     |> List.transpose_exn
-    |> List.map ~f:(fun xs -> 
-        let l = List.length xs in
-        List.foldi xs ~init:(0, l) ~f:(fun idx (acc, curr) x -> 
-            match x with 
-            | 'O' -> (acc + curr), (curr - 1)
-            | '#' -> acc, (l - idx - 1)
-            | _ -> acc, curr
-        )
-    )
-    |> List.map ~f:fst
-    |> List.reduce_exn ~f:( + )
+    |> Tiles.move
+    |> List.transpose_exn
+    |> Tiles.total_load
     |> Int.to_string
 ;;
 
-let part2 file = "part2";;
+
+let part2 file = 
+    let init = Tiles.of_file file in
+    let states = Hashtbl.create (module Tiles) in
+    let rec aux states curr idx =  
+        match Hashtbl.find states curr with 
+        | Some start -> 
+            let cycle_len = idx - start in
+            let result_idx = (Int.rem (1000000000 - start) cycle_len) + start in
+            let result = 
+                Hashtbl.filter_mapi states ~f:(fun ~key ~data ->
+                    if data = result_idx 
+                    then Some (Tiles.total_load key) 
+                    else None
+                )
+                |> Hashtbl.data
+                |> List.hd_exn
+            in
+            result
+        | None -> 
+            Hashtbl.add_exn states ~key:curr ~data:idx;
+            aux states (Tiles.cycle curr) (idx+1)
+    in
+    aux states init 0 |> Int.to_string
+;;
 
 
 let () = 
